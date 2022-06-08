7e44646895d16dc4e41d0da050912606351a9693
Owanikin <oderindeife@gmail.com>
2022-06-08 13:58:49 +0100

    mapping

--- struct.sol
+++ struct.sol
@@ -52,3 +52,19 @@ contract CrowdFunding {
         return true;
     }
 }
+
+contract MappingExample {
+    mapping(address => uint) public balances;
+
+    function update(uint newBalance) public {
+        balances[msg.sender] = newBalance;
+    }
+}
+
+contract MappingUser {
+    function f() public returns (uint) {
+        MappingExample m = new MappingExample();
+        m.update(100);
+        return m.balances(address(this));
+    }
+}
