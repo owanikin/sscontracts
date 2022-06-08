d5c4302e598b06e135fd21dd3722cad4f47b66f2
Owanikin <oderindeife@gmail.com>
2022-06-08 18:05:28 +0100

    explicit conversions

--- iterableMapping.sol
+++ iterableMapping.sol
@@ -105,3 +105,15 @@ contract DeleteExample {
         assert(y.length == 0);
     }
 }
+
+contract C {
+    bytes s = "abcdefgh";
+    function f(bytes calldata c, bytes memory m) public view returns (bytes16, bytes3) {
+        require(c.length == 16, "");
+        bytes16 b = bytes16(m);  // if length of m is greater than 16, truncation will happen
+        b = bytes16(s);   // padded on the right, so result is "abcdefgh\0\0\00\0"
+        bytes3 b1 = bytes3(s);  // truncated, b1 equals to "abc"
+        b = bytes16(c[:8]);  // also padded with zeros
+        return (b, b1);
+    }
+}
