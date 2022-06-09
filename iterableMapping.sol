ee6c7edf1dc987fac946e234b224f7e4a1bdc346
Owanikin <oderindeife@gmail.com>
2022-06-09 14:29:00 +0100

    External function calls

--- iterableMapping.sol
+++ iterableMapping.sol
@@ -117,3 +117,13 @@ contract C {
         return (b, b1);
     }
 }
+
+contract InfoFeed {
+    function info() public payable returns (uint ret) { return 42; }
+}
+
+contract Consumer {
+    InfoFeed feed;
+    function setFeed(InfoFeed addr) public { feed = addr; }
+    function callFeed() public { feed.info{value: 10, gas:800}();}
+}
